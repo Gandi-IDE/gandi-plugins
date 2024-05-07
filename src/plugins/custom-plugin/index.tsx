b6e38add94e2a5d1637ee0fa76d87a382e4fbad5
zhangxiaoqiang <45257469+zxq142857@users.noreply.github.com>
2024-05-07 11:27:13 +0800

    pref: optimizes the loading process of custom plug-ins

--- src/plugins/custom-plugin/index.tsx
+++ src/plugins/custom-plugin/index.tsx
@@ -1,25 +1,72 @@
 import * as React from "react";
+import { noop } from "lodash-es";
 import toast from "react-hot-toast";
 import CustomPluginIcon from "assets/icon--custom-plugin.svg";
+import type { PluginFunction } from "src/plugins-controller";
 
-const CustomPlugin = ({ msg, registerSettings }: PluginContext) => {
+const CustomPlugin = (context: PluginContext) => {
+  const { msg, registerSettings } = context;
   const customPluginScript = React.useRef<HTMLScriptElement>(null);
   const inputElement = React.useRef<HTMLInputElement>(null);
+  const customPlugins = React.useRef<ReturnType<PluginFunction>>({});
 
-  const loadPluginURL = React.useCallback((url: string) => {
-    if (!url) return;
-
-    if (/^(https?|ftp|):\/\/[^\s\/$.?#].[^\s]*\.js$/.test(url)) {
-      if (customPluginScript.current) {
-        document.body.removeChild(customPluginScript.current);
+  const setPluginRegisterToWindow = React.useCallback((loadSuccess: () => void) => {
+    if (!window.Scratch) window.Scratch = {};
+    if (!window.Scratch.plugins) window.Scratch.plugins = {};
+    const removePluginRegisterFromWindow = () => {
+      // Remove the plugin register
+      delete window.Scratch.plugins;
+      if (Object.keys(window.Scratch).length === 0) {
+        delete window.Scratch;
       }
-      customPluginScript.current = document.createElement("script");
-      customPluginScript.current.src = url;
-      document.body.appendChild(customPluginScript.current);
-    } else {
-      toast.error(msg("plugins.pluginsManager.customPlugin.invalidLink"));
-    }
+    };
+    window.Scratch.plugins.register = (plugin: PluginFunction, pluginName = "unknown-plugin") => {
+      const instance = plugin(context);
+      if (customPlugins.current[pluginName]) {
+        customPlugins.current[pluginName]();
+        delete customPlugins.current[pluginName];
+      }
+      customPlugins.current[pluginName] = instance?.dispose || noop;
+      removePluginRegisterFromWindow();
+      loadSuccess();
+    };
+    return removePluginRegisterFromWindow;
   }, []);
+
+  const useScriptTagLoadPlugin = React.useCallback((url: string, loadSuccess: () => void, loadFailed: () => void) => {
+    if (customPluginScript.current) {
+      document.body.removeChild(customPluginScript.current);
+    }
+    customPluginScript.current = document.createElement("script");
+    customPluginScript.current.src = url;
+    const removePluginRegisterFromWindow = setPluginRegisterToWindow(loadSuccess);
+    customPluginScript.current.onerror = (error) => {
+      console.error(`Script file loading failed: ${error}`);
+      removePluginRegisterFromWindow();
+      loadFailed();
+    };
+    document.body.appendChild(customPluginScript.current);
+  }, []);
+
+  const loadPluginFromURL = React.useCallback(
+    (url: string) => {
+      if (!url) return;
+      const pluginLoadPromise = new Promise<void>((resolve, reject) => {
+        if (/^(https?|ftp|):\/\/[^\s\/$.?#].[^\s]*\.js$/.test(url)) {
+          useScriptTagLoadPlugin(url, resolve, reject);
+        } else {
+          toast.error(msg("plugins.pluginsManager.customPlugin.invalidLink"));
+          reject();
+        }
+      });
+      toast.promise(pluginLoadPromise, {
+        loading: msg("plugins.customPlugin.loading"),
+        success: msg("plugins.customPlugin.loadSuccess"),
+        error: msg("plugins.customPlugin.loadFailed"),
+      });
+    },
+    [context],
+  );
 
   React.useEffect(() => {
     const register = registerSettings(
@@ -29,7 +76,6 @@ const CustomPlugin = ({ msg, registerSettings }: PluginContext) => {
         {
           key: "customPlugin",
           label: msg("plugins.customPlugin.title"),
-          // description: msg("plugins.pluginsManager.description"),
           items: [
             {
               key: "pluginFileUrl",
@@ -42,7 +88,7 @@ const CustomPlugin = ({ msg, registerSettings }: PluginContext) => {
                   (e.target as HTMLInputElement).blur();
                 },
                 onBlur: (e) => {
-                  loadPluginURL(e.target.value);
+                  loadPluginFromURL(e.target.value);
                 },
               },
               value: "",
@@ -68,19 +114,11 @@ const CustomPlugin = ({ msg, registerSettings }: PluginContext) => {
                     const pluginLoadPromise = new Promise<void>((resolve, reject) => {
                       const reader = new FileReader();
                       reader.onload = () => {
-                        const script = document.createElement("script");
-                        script.onload = () => {
-                          resolve();
-                        };
-                        script.onerror = (error) => {
-                          console.error(`Script file loading failed: ${error}`);
-                          reject();
-                        };
-                        script.src = reader.result as string;
-                        document.body.appendChild(script);
+                        useScriptTagLoadPlugin(reader.result as string, resolve, reject);
                       };
                       reader.onerror = () => {
                         console.error(`Could not read plugin as data URL: ${reader.error}`);
+                        reject();
                       };
                       reader.readAsDataURL(file);
                     });
@@ -102,6 +140,10 @@ const CustomPlugin = ({ msg, registerSettings }: PluginContext) => {
     );
 
     return () => {
+      const pluginNames = Object.keys(customPlugins.current);
+      pluginNames.forEach((name) => {
+        customPlugins.current[name].dispose();
+      });
       register.dispose();
     };
   }, []);
