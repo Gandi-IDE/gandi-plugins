fb22cd9768017c4a314dcdcc11bb12fddb3a98df
fath11 <fathimkamal@gmail.com>
2024-05-11 09:38:34 +0800

    implemented removeGroundGlass function

--- src/plugins/kukemc-beautify/index.tsx
+++ src/plugins/kukemc-beautify/index.tsx
@@ -98,6 +98,8 @@ const KukemcBeautify: React.FC<PluginContext> = ({ msg, registerSettings }) => {
       <KukemcBeautifyIcon />,
     );
     return () => {
+      ground = false
+      removeGroundGlass()
       register.dispose();
     };
   }, [registerSettings, msg]);
@@ -148,6 +150,53 @@ function groundGlass(): void {
     console.error("Error applying ground glass effect:", error);
   }
 }
+
+function removeGroundGlass(): void {
+  const styleElements = document.head.getElementsByTagName("style");
+
+  const htmlElement = document.documentElement;
+  const computedStyle = window.getComputedStyle(htmlElement);
+  const themeColorHex = computedStyle.getPropertyValue("--theme-color-300").trim(); // Remove any potential whitespace
+
+  const themeColorRgb = hexToRgb(themeColorHex);
+
+  const { r, g, b } = themeColorRgb;
+
+  try {
+    for (let i = 0; i < styleElements.length; i++) {
+      const sheet = styleElements[i].sheet as CSSStyleSheet;
+      if (sheet.cssRules) {
+        for (let j = 0; j < sheet.cssRules.length; j++) {
+          const rule = sheet.cssRules[j] as CSSStyleRule;
+          const isTargetSelector = [
+            ".blocklyWidgetDiv .goog-menu",
+            ".gandi_context-menu_context-menu_2SJM-",
+            ".blocklyToolboxDiv",
+            ".gandi_plugins_plugins-root_xA3t3",
+            ".gandi_collapsible-box_collapsible-box_1_329",
+            ".gandi_target-pane_count_3fmUd",
+            ".gandi_editor-wrapper_tabList_4HFZz",
+            ".gandi_setting-modal_modal-overlay_3wJji",
+            ".gandi_bulletin-modal_modal-overlay_TBAhj",
+            ".gandi_collapsible-box_header_dc9Es",
+          ].includes(rule.selectorText);
+
+          if (rule.selectorText === ".gandi_setting-modal_modal-overlay_3wJji") {
+            applyStyleChanges(rule, `rgba(0,0,0,.7)`, `blur(0)`);
+          } else if (rule.selectorText === ".blocklyToolboxDiv") {
+            applyStyleChanges(rule, `rgba(${r}, ${g}, ${b}, 1)`, `blur(0)`);
+          } else if (isTargetSelector) {
+            applyStyleChanges(rule, `rgba(${r}, ${g}, ${b}, 1)`, `blur(0)`);
+          }
+        }
+      }
+    }
+    styleElement.innerText = `.gandi_collapsible-box_header_dc9Es{border-top-left-radius: 7px;border-top-right-radius: 7px;}`;
+  } catch (error) {
+    console.error("Error removing ground glass effect:", error);
+  }
+}
+
 
 KukemcBeautify.displayName = "KukemcBeautify";
 
