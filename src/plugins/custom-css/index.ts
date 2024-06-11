082f74fcfe634a5709a508ea07c37876ed7aa7fd
zhangxiaoqiang <45257469+zxq142857@users.noreply.github.com>
2024-06-11 20:56:02 +0800

    feat: add some type definitions to the VM and runtime

--- src/plugins/custom-css/index.ts
+++ src/plugins/custom-css/index.ts
@@ -1,9 +1,9 @@
 import CustomCssIcon from "assets/icon--custom-css.svg";
 import React from "react";
-const CustomCss = ({ registerSettings,msg }: PluginContext) => {
-  let linkDom=document.createElement("link");
+const CustomCss = ({ registerSettings, msg }: PluginContext) => {
+  const linkDom = document.createElement("link");
   linkDom.type = "text/css";
-  linkDom.rel = "stylesheet"; 
+  linkDom.rel = "stylesheet";
   linkDom.id = "custom-css";
   document.getElementsByTagName("head")[0].appendChild(linkDom);
   const register = registerSettings(
@@ -11,10 +11,10 @@ const CustomCss = ({ registerSettings,msg }: PluginContext) => {
     "custom-css",
     [
       {
-        key:"custom-css",
+        key: "custom-css",
         label: msg("plugins.customCss.name"),
         description: msg("plugins.customCss.description"),
-        items:[
+        items: [
           {
             key: "load-from-url",
             label: msg("plugins.customCss.load"),
@@ -23,21 +23,21 @@ const CustomCss = ({ registerSettings,msg }: PluginContext) => {
             description: msg("plugins.customCss.load.description"),
             onChange: (value: string) => {
               // var允许重复定义，这里方便一些直接使用
-              var pd = value.substring(0,3)
-              if (pd = "http"){
+              let pd = value.substring(0, 3);
+              if ((pd = "http")) {
                 linkDom.href = value;
               }
-          }
-        }
-        ]
-      }
+            },
+          },
+        ],
+      },
     ],
     React.createElement(CustomCssIcon),
-  )
+  );
   return {
     dispose: () => {
       /** Remove some side effects */
-    register.dispose()
+      register.dispose();
     },
   };
 };
