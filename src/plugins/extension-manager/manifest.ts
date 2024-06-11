5c2d4ac62b36b2445830d70ea86a4b7b5e56e1f7
Fath11 <fathencoded@gmail.com>
2024-06-11 11:37:31 +0800

    Update manifest.ts

--- src/plugins/extension-manager/manifest.ts
+++ src/plugins/extension-manager/manifest.ts
@@ -7,5 +7,9 @@ export default {
       name: "fath11@Cocrea",
       link: "https://cocrea.world/@Fath11",
     },
+    {
+      name: "Cubester@NitroBolt",
+      link: "https://github.com/CubesterYT",
+    },
   ],
 };
