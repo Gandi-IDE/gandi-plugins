fb93dbe119f7b030c3960b4fff248eeb3619076f
zhangxiaoqiang <45257469+zxq142857@users.noreply.github.com>
2024-06-12 15:05:25 +0800

    feat: L10n supports Spanish, Ukrainian, Russian, and Malay languages.

--- src/plugins-l10n.ts
+++ src/plugins-l10n.ts
@@ -1,4 +1,8 @@
 export default {
   en: require("./l10n/en.json"),
+  es: require("./l10n/es.json"),
+  ms: require("./l10n/ms.json"),
+  ru: require("./l10n/ru.json"),
+  uk: require("./l10n/uk.json"),
   "zh-cn": require("./l10n/zh-cn.json"),
 };
