75c49bd4048b8f61717e9a85e4727df3b5d183f9
yuen619 <en5991@outlook.com>
2024-06-29 22:28:25 +0800

    🌈 style(TerminalManager): remove credits space

--- src/plugins/terminal-manager/manifest.ts
+++ src/plugins/terminal-manager/manifest.ts
@@ -4,7 +4,7 @@ export default {
   description: "Manager your terminal.",
   credits: [
     {
-      name: "YUEN @ CCW",
+      name: "YUEN@CCW",
       link: "https://www.ccw.site/student/628979aa804a3a2bc801b097",
     },
   ],
