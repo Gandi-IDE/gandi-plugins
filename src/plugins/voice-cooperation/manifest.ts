a6a1c803243c3b95a36ed77e2c345430f2d4d65d
SparrowHe <sparrowhe@gmail.com>
2024-07-30 17:51:39 +0800

    fix: add authors' link

--- src/plugins/voice-cooperation/manifest.ts
+++ src/plugins/voice-cooperation/manifest.ts
@@ -5,9 +5,11 @@ export default {
   credits: [
     {
       name: "Sparrow",
+      link: "https://www.ccw.site/student/619b9141e0c34311283fd4d8",
     },
     {
       name: "Cappu",
+      link: "https://www.ccw.site/student/62b963fe01fb2c3c96d5d8ad",
     },
   ],
 };
