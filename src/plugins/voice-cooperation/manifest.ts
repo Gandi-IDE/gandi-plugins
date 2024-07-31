5ae15151ad30cd01768126ccc27c491abd7b556b
SparrowHe <sparrowhe@gmail.com>
2024-07-31 16:29:45 +0800

    remove Cappu as required

--- src/plugins/voice-cooperation/manifest.ts
+++ src/plugins/voice-cooperation/manifest.ts
@@ -7,9 +7,5 @@ export default {
       name: "Sparrow",
       link: "https://www.ccw.site/student/619b9141e0c34311283fd4d8",
     },
-    {
-      name: "Cappu",
-      link: "https://www.ccw.site/student/62b963fe01fb2c3c96d5d8ad",
-    },
   ],
 };
