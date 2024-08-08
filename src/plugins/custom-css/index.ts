e44a12c87d4f2df027e94ca98b763d86fa36c146
fath11 <fathimkamal@gmail.com>
2024-08-08 23:04:29 +0800

    turbowarpDark preset theme for customCSS beta

--- src/plugins/custom-css/index.ts
+++ src/plugins/custom-css/index.ts
@@ -1,11 +1,18 @@
 import CustomCssIcon from "assets/icon--custom-css.svg";
 import React from "react";
+
+import presetThemes from "./presetThemes.less";
+
 const CustomCss = ({ registerSettings, msg }: PluginContext) => {
   const linkDom = document.createElement("link");
   linkDom.type = "text/css";
   linkDom.rel = "stylesheet";
   linkDom.id = "custom-css";
   document.getElementsByTagName("head")[0].appendChild(linkDom);
+
+  const removeAllStyles = () => {
+    document.body.classList.remove(presetThemes.turbowarpDark)
+  }
   const register = registerSettings(
     msg("plugins.customCss.title"),
     "custom-css",
@@ -15,6 +22,27 @@ const CustomCss = ({ registerSettings, msg }: PluginContext) => {
         label: msg("plugins.customCss.name"),
         description: msg("plugins.customCss.description"),
         items: [
+          {
+            key: 'presetThemes',
+            type: 'select',
+            label: msg('plugins.customCss.theme'),
+            value: "noPreset",
+            options: [
+              { label: msg('plugins.customCss.theme.none'), value: "noPreset" },
+              { label: msg('plugins.customCss.theme.turbowarpDark'), value: "turbowarpDark" },
+            ],
+            onChange: (value) => {
+              switch(value) {
+                default:
+                  removeAllStyles()
+                  break;
+                case "turbowarpDark":
+                  removeAllStyles()
+                  document.body.classList.add(presetThemes.turbowarpDark)
+                  break;
+              }
+            },
+          },
           {
             key: "load-from-url",
             label: msg("plugins.customCss.load"),
