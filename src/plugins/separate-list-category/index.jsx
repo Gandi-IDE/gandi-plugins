157d612dabbe33cd431ec88d61271f5d74d078b8
fath11 <fathimkamal@gmail.com>
2024-09-18 14:23:47 +0800

    initial

---
+++ src/plugins/separate-list-category/index.jsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import styles from "./styles.less";
+
+const SeparateListCategory = () => {
+  return <React.Fragment>{"This is a new plugin."}</React.Fragment>;
+};
+
+SeparateListCategory.displayName = "Separate list category";
+
+export default SeparateListCategory;
