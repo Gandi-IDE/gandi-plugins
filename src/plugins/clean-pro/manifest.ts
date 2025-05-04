02ab937c64cc3ac6a37afda280130e4c5de4052d
MoreBugOfDog <MoreBugOfDog@outlook.com>
2025-05-04 23:19:17 +0800

    feat(clean-pro):整理积木pro插件，支持一键删除无头块(右键菜单触发)

---
+++ src/plugins/clean-pro/manifest.ts
@@ -0,0 +1,11 @@
+export default {
+  name: "clean-pro",
+  type: "component",
+  description: "Clean Up Blocks Pro!! Quickly clear annoying headless blocks~",
+  credits: [
+    {
+      name: "多bug的啸天犬 @ CCW",
+      link: "https://www.ccw.site/student/197354885",
+    },
+  ],
+};
