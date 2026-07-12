ec2cb4a26277837604300d18fee08edd5a96e044
MengFuziComing~ <121215648+BenPaoDeXiaoZhi@users.noreply.github.com>
2026-07-12 10:16:50 +0800

    Fix(manifest.ts): chen-jin发现了此bug，将他加入协作者

--- src/plugins/debugger-addon/manifest.ts
+++ src/plugins/debugger-addon/manifest.ts
@@ -7,5 +7,9 @@ export default {
       name: "孟夫子驾到@CCW",
       link: "https://www.ccw.site/student/63c2807d669fa967f17f5559",
     },
+    {
+      name: "Chen-Jin@CCW",
+      link: "https://www.ccw.site/student/692538ef86bbc77f84e3b259",
+    }
   ],
 };
